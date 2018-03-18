c994eb0dca92b7012c22bdf72d2736cad9c170ea
J888 <0xjohnh@gmail.com>
2018-03-18 13:04:29 -0400

    almost there

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -12,32 +12,26 @@
   */
 
 
-void fill_matrix(double *matrix, int n, int m, FILE *fp)
-
-
-
 int main(int argc, char* argv[])
 {
-    double *aa;	/* the A matrix */
-    double *bb;	/* the B matrix */
-    double *cc1;	/* A x B computed using the omp-mpi code you write */
-    double *cc2;	/* A x B computed using the conventional algorithm */
-    int myid, numprocs, nrows, ncols;
+
+    int numtasks,taskid,numworkers,source,dest,rows,offset,i,j,k;
     double starttime, endtime;
     MPI_Status status;
-    /* insert other global variables here */ 
-    int i, j, k, rows, offset, dest, source, 
-        arows, acols, brows, bcols, numworkers,
-        totalrows;   
+
+
     FILE *fileA;
     FILE *fileB;
     FILE *fileC;
+
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
-    if (argc == 3){
+
+    if (argc == 3)
+    {
 
         fileA = fopen(argv[1], "r");
         fileB = fopen(argv[2], "r");
@@ -46,11 +40,9 @@ int main(int argc, char* argv[])
         fscanf(fileA, "%d", &arows);
         fscanf(fileA, "%d", &acols);
 
-
         //read SECOND file, get # rows, cols
         fscanf(fileB, "%d", &brows);
         fscanf(fileB, "%d", &bcols);
-        printf("%d, %d", brows, bcols);
 
 
         if (acols == brows){
@@ -61,67 +53,58 @@ int main(int argc, char* argv[])
             return 0;
         }
 
-        aa = malloc(sizeof(double) * arows * acols);
-        bb = malloc(sizeof(double) * brows * bcols);
-        gen_matrix(aa, arows, acols, fileA);
-        gen_matrix(bb, brows, bcols, fileB);
-        cc1 = malloc(sizeof(double) * nrows * ncols);
-        double a[arows][acols];
-        double b[brows][bcols];
-        double c[nrows][ncols];
-        double c2[nrows][ncols];
+        double aa[arows][acols],bb[brows][bcols],cc1[nrows][ncols], cc2[nrows][ncols];
 
-        i = j = 0;
+
+        fscanf(fileA, "%*[^\n]\n"); //skip line 1
         for (i = 0; i < arows; i++){
             for (j = 0; j < acols; j++){
-                if (!fscanf(fileA, "%lf", &a[i][j])) break;
+                if (!fscanf(fileA, "%lf", &aa[i][j])) break;
             }
         }	
         rewind(fileA);
 
+
+        fscanf(fileB, "%*[^\n]\n"); //skip line 1
         for (i = 0; i < brows; i++){
             for (j = 0; j < bcols; j++){
-                if (!fscanf(fileB, "%lf", &b[i][j])) break;
+                if (!fscanf(fileB, "%lf", &bb[i][j])) break;
             }
         }	
         rewind(fileB);
 
+
         if (myid == 0) {  //MASTER CODE
+
             starttime = MPI_Wtime();
+
             offset = 0;
-            numworkers = numprocs-1;
             rows = nrows/numworkers;
-            if (rows == 0) rows = 1;
-            int averow, extra;
-            averow = nrows/numworkers;
-            extra = nrows%numworkers;
+            numworkers = numprocs-1;
+      
             for (dest=1; dest<=min(numworkers, nrows); dest++){
-                rows = (dest <= extra) ? averow+1 : averow;
                 MPI_Send(&offset, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
                 MPI_Send(&rows, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
-                MPI_Send(&a[offset][0], rows*nrows, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
-                MPI_Send(&b, brows*bcols, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
+                MPI_Send(&aa[offset][0], rows*nrows, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
+                MPI_Send(&bb, brows*bcols, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
                 offset = offset + rows;
-                totalrows -= rows;
             } 
 
             for (i=1; i<=min(numworkers, nrows); i++){
                 source = i;
                 MPI_Recv(&offset, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
                 MPI_Recv(&rows, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
-
                 MPI_Recv(&c[offset][0], rows*nrows, MPI_DOUBLE, source, 2, MPI_COMM_WORLD, &status);
             }
             endtime = MPI_Wtime();
-            printf("%f\n", (endtime - starttime));
-            //      mmult(cc2, aa, arows, acols, bb, brows, bcols); 
+
+            //do the mmult the old way and put result in cc2
             for (i=0; i<nrows; i++){
                 for (j=0; j<ncols; j++){
-                    c2[i][j] = 0;
+                    cc2[i][j] = 0;
                     for (k=0; k<nrows; k++){
-                        c2[i][j] += a[i][k] * b[k][j];
+                        cc2[i][j] += aa[i][k] * bb[k][j];
                     }
-
                 }
             }
 
@@ -130,16 +113,17 @@ int main(int argc, char* argv[])
             for (i=0; i<nrows; i++){
                 for (j=0; j<ncols; j++){
                     if (c[i][j] != c2[i][j]) isSame = 0;
-                    printf("c[%d][%d] = %lf c2[%d][%d] = %lf\n", i, j, c[i][j], i, j, c2[i][j]);
+                    printf("c[%d][%d] = %lf c2[%d][%d] = %lf\n", i, j, cc1[i][j], i, j, cc2[i][j]);
                 }
             }
+
             if (isSame){
                 printf("These matrices are the same.\n");
             } else {
                 printf("These matrices are NOT the same.\n");
             }
 
-            fileC = fopen("matrixC", "w");
+            fileC = fopen("matrixC.txt", "w");
             char space[] = " ";
             char nline[] = "\n";
             char s[20];
@@ -151,16 +135,18 @@ int main(int argc, char* argv[])
                 }
                 fwrite(nline, 1, sizeof(char), fileC); 
             }
-            printf("MatrixC has been stored in file: matrixC.\n");
+            printf("MatrixC has been stored in file: matrixC.txt\n");
 
-        }else { //SLAVE CODE
+
+        } 
+        else if(myid > 0) 
+        { //SLAVE CODE
       
-            if (nrows >= myid){
-                source = 0;
-                MPI_Recv(&offset, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
-                MPI_Recv(&rows, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);	
-                MPI_Recv(&a, rows*nrows, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
-                MPI_Recv(&b, brows*bcols, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
+            source = 0;
+            MPI_Recv(&offset, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+            MPI_Recv(&rows, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);	
+            MPI_Recv(&aa, rows*nrows, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
+            MPI_Recv(&bb, brows*bcols, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
              
 #pragma omp parallel default(none)  \
     shared(a, b, c, ncols, rows, brows) private(i, k, j)
@@ -176,34 +162,17 @@ int main(int argc, char* argv[])
                 printf("Multiplication from worker %d complete!\n", myid);
                 MPI_Send(&offset, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
                 MPI_Send(&rows, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-                MPI_Send(&c, rows*ncols, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
-            }
+                MPI_Send(&cc1, rows*ncols, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+           
         }
-    } else {
+    }
+    else
+    {
         fprintf(stderr, "Two files with MatrixA and MatrixB must be passed as arguments\n");
         return 0;
     }
-    MPI_Finalize();
 
+    MPI_Finalize();
     return 0;
 }
-
-
-
-
-void fill_matrix(double *matrix, int n, int m, FILE *fp) {    
-    
-    fscanf(config_file, "%*[^\n]\n", NULL);//skip first line of file
-
-    int i, j;
-    double c;
-    i = j = 0;
-    for (i = 0; i < n; i++){
-        for (j = 0; j < m; j++){
-            if (!fscanf(fp, "%lf", &matrix[n*i+j])) break;
-        }
-    }   
-    rewind(fp);
-}
-
 
